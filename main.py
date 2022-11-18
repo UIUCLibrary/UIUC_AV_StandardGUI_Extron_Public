d207f74884ea45c774d07456b743c3e2dd3845f5
Neil Feuerhelm <feuerhe1@illinois.edu>
2022-11-18 11:56:32 -0600

    Copies initial content of main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+## Begin ControlScript Import --------------------------------------------------
+from extronlib import event, Version
+from extronlib.device import eBUSDevice, ProcessorDevice, UIDevice
+from extronlib.interface import (CircuitBreakerInterface, ContactInterface,
+    DigitalInputInterface, DigitalIOInterface, EthernetClientInterface,
+    EthernetServerInterfaceEx, FlexIOInterface, IRInterface, PoEInterface,
+    RelayInterface, SerialInterface, SWACReceptacleInterface, SWPowerInterface,
+    VolumeInterface)
+from extronlib.ui import Button, Knob, Label, Level, Slider
+from extronlib.system import Clock, MESet, Timer, Wait, File, SaveProgramLog
+from dateime import datetime
+
+print(Version())
+
+## End ControlScript Import ----------------------------------------------------
+##
+## Begin User Import -----------------------------------------------------------
+
+## End User Import -------------------------------------------------------------
+##
+## Begin Device/Processor Definition -------------------------------------------
+
+CtlProc_Main = ProcessorDevice('CTL001')
+
+## End Device/Processor Definition ---------------------------------------------
+##
+## Begin Device/User Interface Definition --------------------------------------
+
+TP_Main = UIDevice('TP001')
+
+## End Device/User Interface Definition ----------------------------------------
+##
+## Begin Communication Interface Definition ------------------------------------
+
+## End Communication Interface Definition --------------------------------------
+
+def Initialize():
+    TP_Main.ShowPage('Splash')
+    
+    ## DO ADDITIONAL STARTUP ITEMS HERE
+    
+    print('System Initialized')
+
+## Event Definitions -----------------------------------------------------------
+
+## End Events Definitions-------------------------------------------------------
+
+Initialize()
